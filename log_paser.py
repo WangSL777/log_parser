e9d9336e12cf0cccbe74e320b309fd4fcf2f8032
Shuliang Wang <shw@mega.co.nz>
2024-09-16 22:50:07 +1200

    add side, symbol, ip, message queue stats

--- log_paser.py
+++ log_paser.py
@@ -24,10 +24,19 @@ class LogParser:
         self.error_event_with_trace = []
         self.events_time_delta = None
         self.order_request_events = []
+        self.buy_request_events = []
+        self.sell_request_events = []
         self.accepted_order_request_events = []
         self.accepted_order_request_type_count = {}
         self.reject_order_request_events = []
         self.reject_order_request_type_count = {}
+        self.order_request_symbol_count = {}
+        self.order_request_client_ip_count = {}
+        self.rmq_send_events = []
+        self.rmq_send_queue_size_events = []
+        self.rmq_recv_events = []
+        self.rmq_recv_callback_process_queue_size_events = []
+        self.rmq_recv_msg_dist_runnable_process_queue_size_events = []
 
     def parse_events(self, start_datetime=None, end_datetime=None):
         """Parse events.
@@ -70,6 +79,20 @@ class LogParser:
                             self.accepted_order_request_type_count[event['order_request_status']] += 1
                         else:
                             self.accepted_order_request_type_count[event['order_request_status']] = 1
+
+                        # side
+                        if event['side'] == 'BUY':
+                            self.buy_request_events.append(event)
+                        elif event['side'] == 'SELL':
+                            self.sell_request_events.append(event)
+                        else:
+                            log.warning(f'Get unknown side type "{event["side"]}"')
+
+                        # symbol
+                        if event['symbol'] in self.order_request_symbol_count:
+                            self.order_request_symbol_count[event['symbol']] += 1
+                        else:
+                            self.order_request_symbol_count[event['symbol']] = 1
                     else:
                         self.reject_order_request_events.append(event)
                         key = f"http_status:{event['http_status']} - reject_code:{event['reject_code']} - reject_message:{event['reject_message']}"
@@ -77,6 +100,22 @@ class LogParser:
                             self.reject_order_request_type_count[key] += 1
                         else:
                             self.reject_order_request_type_count[key] = 1
+
+                    # client ip
+                    if event['client_ip'] in self.order_request_client_ip_count:
+                        self.order_request_client_ip_count[event['client_ip']] += 1
+                    else:
+                        self.order_request_client_ip_count[event['client_ip']] = 1
+                elif 'type' in event and event['type'] == 'rmq_sent':
+                    self.rmq_send_events.append(event)
+                elif 'type' in event and event['type'] == 'rmq_send_queue_size':
+                    self.rmq_send_queue_size_events.append(event)
+                elif 'type' in event and event['type'] == 'rmq_receive':
+                    self.rmq_recv_events.append(event)
+                elif 'type' in event and event['type'] == 'rmq_recv_callback_process_queue_size':
+                    self.rmq_recv_callback_process_queue_size_events.append(event)
+                elif 'type' in event and event['type'] == 'rmq_msg_dist_runnable_process_queue_size':
+                    self.rmq_recv_msg_dist_runnable_process_queue_size_events.append(event)
 
         self.events_time_delta = self.events[-1]['timestamp'] - self.events[0]['timestamp']
 
@@ -134,6 +173,58 @@ class LogParser:
             else:
                 log.warning(f'Cannot find process_time: {line}')
 
+            # "side":"BUY", side:BUY
+            pattern = r".*(\")?side(\")?:(\")?(?P<side>\w+)(\")?.*"
+            match = re.search(pattern, message)
+            if match:
+                parsed_log['side'] = match.group("side")
+            elif parsed_log['http_status'] == '200':
+                log.warning(f'Cannot find side: {line}')
+
+            # "symbol":"1539240640083"
+            pattern = r".*\"symbol\":\"(?P<symbol>\w+)\".*"
+            match = re.search(pattern, message)
+            if match:
+                parsed_log['symbol'] = match.group("symbol")
+            elif parsed_log['http_status'] == '200':
+                log.warning(f'Cannot find symbol: {line}')
+
+            # client ip, [FROM]: [114.236.238.230]
+            pattern = r".*\[FROM\]: \[(?P<client_ip>\d+\.\d+\.\d+\.\d+)\].*"
+            match = re.search(pattern, message)
+            if match:
+                parsed_log['client_ip'] = match.group("client_ip")
+            else:
+                log.warning(f'Cannot find client_ip: {line}')
+        elif "SEND THREAD: cId:" in line and "SENT" in line:
+            parsed_log['type'] = 'rmq_sent'
+        elif "Send thread's send queue size:" in line:
+            parsed_log['type'] = 'rmq_send_queue_size'
+            pattern = r".*Send thread's send queue size: (?P<rmq_send_queue_size>\d+).*"
+            match = re.search(pattern, message)
+            if match:
+                parsed_log['rmq_send_queue_size'] = int(match.group("rmq_send_queue_size"))
+            else:
+                log.warning(f'Cannot find rmq_send_queue_size: {line}')
+        elif "RECV of Message Envelope cId:" in line:
+            parsed_log['type'] = 'rmq_receive'
+        elif "Recv Processor's CallbackProcessQueue size:" in line:
+            parsed_log['type'] = 'rmq_recv_callback_process_queue_size'
+            pattern = r".*Recv Processor's CallbackProcessQueue size: (?P<rmq_recv_callback_process_queue_size>\d+).*"
+            match = re.search(pattern, message)
+            if match:
+                parsed_log['rmq_recv_callback_process_queue_size'] = int(match.group("rmq_recv_callback_process_queue_size"))
+            else:
+                log.warning(f'Cannot find rmq_recv_callback_process_queue_size: {line}')
+        elif "RecvMsgDistRunnable's processQueue size:" in line:
+            parsed_log['type'] = 'rmq_msg_dist_runnable_process_queue_size'
+            pattern = r".*RecvMsgDistRunnable's processQueue size: (?P<rmq_recv_msg_dist_runnable_process_queue_size>\d+).*"
+            match = re.search(pattern, message)
+            if match:
+                parsed_log['rmq_recv_msg_dist_runnable_process_queue_size'] = int(match.group("rmq_recv_msg_dist_runnable_process_queue_size"))
+            else:
+                log.warning(f'Cannot find rmq_recv_msg_dist_runnable_process_queue_size: {line}')
+
         return parsed_log
 
     def get_timestamp(self, event_type, last=False):
@@ -179,25 +270,54 @@ class LogParser:
         summary.append(f'Error event count: {len(self.error_events)}')
         summary.append((f'Error event type and count: {json.dumps(self.error_events_type_count, indent=4)}'))
         summary.append(f'Error event with exception trace count: {len(self.error_event_with_trace)}')
-        summary.append(f'Event duration: {self.events_time_delta}')
-        summary.append((f'Order requests count: {len(self.order_request_events)}'))
+        summary.append(f'Parsed log duration: {self.events_time_delta}')
+        summary.append((f'\nOrder requests count: {len(self.order_request_events)}'))
         summary.append((f'Accepted order requests count: {len(self.accepted_order_request_events)}'))
         summary.append((f'Accepted order requests status type and count: {json.dumps(self.accepted_order_request_type_count, indent=4)}'))
         summary.append((f'Rejected order requests count: {len(self.reject_order_request_events)}'))
         summary.append((f'Rejected order requests status type and count: {json.dumps(self.reject_order_request_type_count, indent=4, sort_keys=True)}'))
 
-        summary.append(f'Average order request rate: {len(self.order_request_events) / self.events_time_delta.total_seconds()} requests per second')
-        summary.append(f'Average order request process time: {statistics.mean([x["process_time_ms"] for x in self.order_request_events])} ms')
-        # min
-        # max
+        summary.append(f'\nAverage order request rate: {len(self.order_request_events) / self.events_time_delta.total_seconds()} requests per second')
+        process_times = [x["process_time_ms"] for x in self.order_request_events]
+        summary.append(f'Average order request process time: {statistics.mean(process_times)} ms')
+        summary.append(f'Min order request process time: {min(process_times)} ms')
+        summary.append(f'Max order request process time: {max(process_times)} ms')
 
-        # sell or buy distribution
+        # sell or buy
+        summary.append((f'\nSell requests count: {len(self.sell_request_events)}'))
+        summary.append(f'Average sell request rate: {len(self.sell_request_events) / self.events_time_delta.total_seconds()} requests per second')
+        process_times = [x["process_time_ms"] for x in self.sell_request_events]
+        summary.append(f'Average sell request process time: {statistics.mean(process_times)} ms')
+        summary.append(f'Min sell request process time: {min(process_times)} ms')
+        summary.append(f'Max sell request process time: {max(process_times)} ms')
+        summary.append((f'\nBuy requests count: {len(self.buy_request_events)}'))
+        summary.append(f'Average buy request rate: {len(self.buy_request_events) / self.events_time_delta.total_seconds()} requests per second')
+        process_times = [x["process_time_ms"] for x in self.buy_request_events]
+        summary.append(f'Average buy request process time: {statistics.mean(process_times)} ms')
+        summary.append(f'Min buy request process time: {min(process_times)} ms')
+        summary.append(f'Max buy request process time: {max(process_times)} ms')
+
         # symbol distribution
-        # unique ip address
+        summary.append((f'\nOrder requests symbol and count: {json.dumps(self.order_request_symbol_count, indent=4)}'))
 
-        # Message Queue Statistics
-        #
-        # Average Send Queue Size: 0 (based on available data)
-        # Average Recv Processor's CallbackProcessQueue Size: 1 (based on available data)
-        # Average RecvMsgDistRunnable's ProcessQueue Size: 0 (based on available data)
+        # unique ip address
+        summary.append((f'\nOrder requests client IP and count: {json.dumps(self.order_request_client_ip_count, indent=4)}'))
+
+        # Message Queue Stats
+        summary.append((f'\nRMQ sent event count: {len(self.rmq_send_events)}'))
+        queue_size = [x["rmq_send_queue_size"] for x in self.rmq_send_queue_size_events]
+        summary.append(f'Average RMQ sent queue size: {statistics.mean(queue_size)}')
+        summary.append(f'Min RMQ sent queue size: {min(queue_size)}')
+        summary.append(f'Max RMQ sent queue size: {max(queue_size)}')
+
+        summary.append((f'\nRMQ receive event count: {len(self.rmq_recv_events)}'))
+        queue_size = [x["rmq_recv_callback_process_queue_size"] for x in self.rmq_recv_callback_process_queue_size_events]
+        summary.append(f'Average RMQ receive callback process queue size: {statistics.mean(queue_size)}')
+        summary.append(f'Min RMQ receive callback process queue size: {min(queue_size)}')
+        summary.append(f'Max RMQ receive callback process queue size: {max(queue_size)}')
+        queue_size = [x["rmq_recv_msg_dist_runnable_process_queue_size"] for x in self.rmq_recv_msg_dist_runnable_process_queue_size_events]
+        summary.append(f'Average RMQ receive msg dist runnable process queue size: {statistics.mean(queue_size)}')
+        summary.append(f'Min RMQ receive msg dist runnable process queue size: {min(queue_size)}')
+        summary.append(f'Max RMQ receive msg dist runnable process queue size: {max(queue_size)}')
         log.info('\n'.join(summary))
+
